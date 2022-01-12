308c1cb7d646ad8f0e48fecb3c5e14bd3c255957
Eli <eli.h.schei@outlook.com>
2022-01-12 18:53:39 +0100

    Added some checks to avoid error with empty GUID if a list is not selected

--- webparts/react-stickynotes/src/webparts/reactDraggableStickynoteWebpart/ReactDraggableStickynoteWebpartWebPart.ts
+++ webparts/react-stickynotes/src/webparts/reactDraggableStickynoteWebpart/ReactDraggableStickynoteWebpartWebPart.ts
@@ -1,50 +1,59 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 
-import * as strings from 'ReactDraggableStickynoteWebpartWebPartStrings';
+import * as strings from "ReactDraggableStickynoteWebpartWebPartStrings";
 
 import {
   PropertyFieldListPicker,
   PropertyFieldListPickerOrderBy,
 } from "@pnp/spfx-property-controls/lib/PropertyFieldListPicker";
-import StickyNotesBoard, { IStickyNotesBoardProps } from './components/StickyNotesBoard';
-import { IStickyNotesProps } from './components/StickyNote';
-import DataProvider from './data/dataProvider';
+import StickyNotesBoard, {
+  IStickyNotesBoardProps,
+} from "./components/StickyNotesBoard";
+import { IStickyNotesProps } from "./components/StickyNote";
+import DataProvider from "./data/dataProvider";
 
 export interface IReactDraggableStickynoteWebpartProps {
-  title: string,
+  title: string;
   description: string;
   lists: string | string[];
 }
 
 export default class ReactDraggableStickynoteWebpart extends BaseClientSideWebPart<IReactDraggableStickynoteWebpartProps> {
-
   public render(): void {
-    this.getListItems().then((results) => {
-      console.log(results);
-      const element: React.ReactElement<IStickyNotesBoardProps> =
-        React.createElement(StickyNotesBoard, {
-          title: this.properties.title,
-          boardDescription: this.properties.description,
-          listItems: results,
-        });
-
-      ReactDom.render(element, this.domElement);
-    });
+    this.getListItems()
+      .then((results) => {      
+        if (results) {
+          const element: React.ReactElement<IStickyNotesBoardProps> =
+            React.createElement(StickyNotesBoard, {
+              title: this.properties.title,
+              boardDescription: this.properties.description,
+              listItems: results,
+            });
+          ReactDom.render(element, this.domElement);
+        }else{
+          const element =
+            React.createElement("h2", {}, "You must select a list");
+          ReactDom.render(element, this.domElement);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   private async getListItems(): Promise<Array<IStickyNotesProps>> {
     const dataProvider = new DataProvider();
+    const listGUID = this.properties.lists ? this.properties.lists.toString() : ""
     const items = await dataProvider.getListItems(
-      this.properties.lists.toString()
+      listGUID
     );
-
     return items;
   }
 
@@ -53,7 +62,7 @@ export default class ReactDraggableStickynoteWebpart extends BaseClientSideWebPa
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -61,14 +70,14 @@ export default class ReactDraggableStickynoteWebpart extends BaseClientSideWebPa
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
                 }),
                 PropertyFieldListPicker("lists", {
                   label: "Select a list",
@@ -83,11 +92,11 @@ export default class ReactDraggableStickynoteWebpart extends BaseClientSideWebPa
                   deferredValidationTime: 0,
                   key: "listPickerFieldId",
                 }),
-              ]
-            }
-          ]
-        }
-      ]
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
--- webparts/react-stickynotes/src/webparts/reactDraggableStickynoteWebpart/data/dataProvider.ts
+++ webparts/react-stickynotes/src/webparts/reactDraggableStickynoteWebpart/data/dataProvider.ts
@@ -1,4 +1,4 @@
-
+import { Guid } from "@microsoft/sp-core-library";
 import { sp } from "@pnp/sp";
 // import "@pnp/sp/webs";
 // import "@pnp/sp/lists";
@@ -6,33 +6,30 @@ import { sp } from "@pnp/sp";
 import { IStickyNotesProps } from "../components/StickyNote";
 
 export default class DataProvider {
+  public initDataProvider(context) {
+    sp.setup({
+      spfxContext: context,
+    });
+  }
 
-    public initDataProvider(context){
-        sp.setup({
-            spfxContext: context
-          });
+  public async getListItems(
+    listGUID: string
+  ): Promise<Array<IStickyNotesProps>> {
+    // get all the items from a list
+    if (listGUID) {
+      const items: any[] = await sp.web.lists.getById(listGUID).items.get();
+      const mappedItems = items.map((item) => {
+        return {
+          title: item.Title,
+          content: item.Content,
+          id: item.GUID,
+        } as IStickyNotesProps;
+      });
+      return mappedItems;
+    } else {
+        return null;
     }
+  }
 
-    public async getListItems(listGUID: string): Promise<Array<IStickyNotesProps>>{
-        // get all the items from a list
-        // const items: any[] = await sp.web.lists.getById(listGUID).items.get();
-        // const mappedItems = items.map((item) => {
-        //     return {
-        //         title: item.Title,
-        //         content: item.Content,
-        //         id: item.GUID
-
-        //     }as IStickyNotesProps;
-        // })
-        // return mappedItems;
-        return [
-            {content: "Some text here", title: "Some Title here", id: 1} as IStickyNotesProps,
-            {content: "Some other text here", title: "Some other Title here", id: 1} as IStickyNotesProps
-        ]
-    }
-
-    public saveListItems(){
-
-    }
-
+  public saveListItems() {}
 }
